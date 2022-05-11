cba8ddd47f6568243cf57102abba4ec5d3fc834b
Zharkyn20 <61348606+Zharkyn20@users.noreply.github.com>
2022-05-11 17:08:42 +0600

    bugfix/add_requirements.txt

--- video/settings.py
+++ video/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_swagger',
+#   'rest_framework_swagger',
     'corsheaders',
     'drf_yasg',
     'apps.media',
